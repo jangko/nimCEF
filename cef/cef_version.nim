ce116e90183e66cbc43b56461f676cf079bacec3
andri lim <jangko128@gmail.com>
2016-06-20 20:18:39 +0700

    update cef_version

--- cef/cef_version.nim
+++ cef/cef_version.nim
@@ -1,16 +1,16 @@
 include cef_import
 
 const
-  CEF_VERSION*       = "3.2623.1395.g3034273"
+  CEF_VERSION*       = "3.2704.1427.g95055fe"
   CEF_VERSION_MAJOR* = 3
-  CEF_COMMIT_NUMBER* = 1395
-  CEF_COMMIT_HASH*   = "3034273cb99755b2da07955b1b87ae9c2d035244"
+  CEF_COMMIT_NUMBER* = 1427
+  CEF_COMMIT_HASH*   = "95055fe5c355a899dbd6f4fca3f6bae68f80055b"
   COPYRIGHT_YEAR*    = 2016
 
-  CHROME_VERSION_MAJOR* = 49
+  CHROME_VERSION_MAJOR* = 51
   CHROME_VERSION_MINOR* = 0
-  CHROME_VERSION_BUILD* = 2623
-  CHROME_VERSION_PATCH* = 87
+  CHROME_VERSION_BUILD* = 2704
+  CHROME_VERSION_PATCH* = 84
 
   # The API hash is created by analyzing CEF header files for C API type
   # definitions. The hash value will change when header files are modified
@@ -18,18 +18,18 @@ const
   # universal hash value will change if any platform is affected whereas the
   # platform hash values will change only if that particular platform is
   # affected.
-  CEF_API_HASH_UNIVERSAL* = "32c1d3523da124f2dea7b80b92c53c4d4a463c65"
+  CEF_API_HASH_UNIVERSAL* = "a4358963bc66adefedbf3008a83007e89afffab9"
 
 
 when defined(windows):
   const
-    CEF_API_HASH_PLATFORM* = "64b27477b82b44b51ce817522f744fca6768cbbb"
+    CEF_API_HASH_PLATFORM* = "770131916655f914b4659d82ef08993bf9cfdc22"
 elif defined(MACOSX):
   const
-    CEF_API_HASH_PLATFORM* = "e3b9c36454ae5ae4fb3509e17fb6a7d2877c847d"
+    CEF_API_HASH_PLATFORM* = "95517cba92239cc69c4267a649c6dad0781f245c"
 elif defined(UNIX):
   const
-    CEF_API_HASH_PLATFORM* = "87a195efc055fb9f39c84f5ce8199cc8766290e3"
+    CEF_API_HASH_PLATFORM* = "5963da3614d469320c9cc726ce7d838280132c26"
 
 # Returns CEF version information for the libcef library. The |entry|
 # parameter describes which version component will be returned:
